cc331559365bc1c436aca904575ac873855be6dd
Tkamunya1 <titus.kamunya@student.moringaschool.com>
2022-06-02 16:15:19 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(q,s){
+return q+s;
+}
+function subtract(q,s) {
+return q-s
+}
+function multiply(q,s){
+return q*s
+}
+function divide(q,s){
+return q/s
+}
+function increment(q){
+return q+1
+}
+function decrement (q){
+    return q-1
+}
+function makeInt(string){
+    return parseInt(string,10)
+}
+function preserveDecimal(string){
+    return parseInt(string)
+}
+function preserveDecimal(string){
+    return parseFloat(string)
+}
